a0267df13a4c0f1a847412f6094bfffe528ea9a0
Cristina Calvo <xmcaldia327@ieshnosmachado.org>
2023-10-06 11:22:40 +0200

    Visualizamos json

--- proyecto.py
+++ proyecto.py
@@ -1,2 +1,10 @@
-#Pruebas
-print("Adios")
+
+# Importamos el modulo de request
+import requests
+ 
+# Creamos el request que queremos con nuestra url de la API
+response = requests.get('https://ideihm.covam.es/api-ihm/getmarea?request=gettide&amp;id=42&amp;format=json&amp;month=202310')
+ 
+# Visualizamos el contenido del JSON
+print(response.json())
+
