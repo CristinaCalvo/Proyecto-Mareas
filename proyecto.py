8bf8247ab0163c79276db14282199dba1608d5be
Cristina Calvo <xmcaldia327@ieshnosmachado.org>
2023-10-06 13:14:59 +0200

    Comprobamos el tipo de objeto de mareas

--- proyecto.py
+++ proyecto.py
@@ -1,10 +1,28 @@
 
 # Importamos el modulo de request
 import requests
+
+# Importamos el modulo json
+import json
  
 # Creamos el request que queremos con nuestra url de la API
-response = requests.get('https://ideihm.covam.es/api-ihm/getmarea?request=gettide&amp;id=42&amp;format=json&amp;month=202310')
- 
-# Visualizamos el contenido del JSON
-print(response.json())
+pagina = requests.get('https://ideihm.covam.es/api-ihm/getmarea?request=gettide&amp;id=42&amp;format=json&amp;month=202310')
 
+# Visualizamos el contenido del JSON
+#print(pagina.json()) #Esto ya no lo utilizamos ya que queremos visualizarlo ordenadamente 
+
+# Creamos la variable fichero para meter los datos
+fichero = pagina.json()
+
+# print(fichero) #Comprobamos que fichero es realmente los datos que necesitamos
+
+print(type(fichero['mareas']))
+
+# Tratamos el fichero JSON
+#for mareas in fichero['mareas']:
+ #  print(mareas['puerto'])
+
+
+
+   # for datos in mareas_dict['datos']:
+    #print(datos['fecha'] + datos['hora'] + datos['altura'] + datos['tipo'])
